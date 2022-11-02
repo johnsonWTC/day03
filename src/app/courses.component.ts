c5773a241773f57fade30ec9380ae47881e7d542
/ohnsonWTC <johnsondubula@gmail.com>
2022-11-02 21:51:26 +0200

    component with template and selector

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,3 +1,11 @@
-class CoursesComponent{
-    
+import { Component } from "@angular/core";   
+
+
+@Component({
+    selector: 'courses',
+    template : '<h1>courses</h1>'
+})
+
+export class CoursesComponent{
+
 }
